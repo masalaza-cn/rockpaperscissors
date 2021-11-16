baa30bdca5f255abb3e008831efd101399a0b1b5
Glitch (rps-dropbox-mika) <none>
2021-11-16 23:23:30 +0000

    ⛰💒 Updated with Glitch 11.16

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
+//Rock, Paper, Scissors Game
 
+(".play").click(function () {
+
+  let userChoice = $(".input").val();
+  $(".userChoice").text(userChoice);
+ 
+  
+  
+  
+  
+//Possible Responses
+
+
+
+
+
+//Computer Choices
+
+                
+//User v. Computer
+  
+  
+  
+  
+  
+
+  
+});
+
+  
